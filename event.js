cdc811d10830f23b8fcfb875a66ebf3af3da7f6b
Sandip Neupane <sandipnyoupane45@gmail.com>
2023-10-01 19:21:00 +0545

    js

--- event.js
+++ event.js
@@ -1,9 +1,9 @@
-// console.log(document.body);
-// let div = document.getElementById("hero-section");
-// div.innerHTML="Hello";
+console.log(document.body);
+let div = document.getElementById("hero-section");
+div.innerHTML="Hello";
 
-// const paragraphs= document.getElementsByClassName("para")[2];
-// paragraphs.innerHTML="<b>changed</b>";
+const paragraphs= document.getElementsByClassName("para")[2];
+paragraphs.innerHTML="<b>changed</b>";
 
 const colorlist=["red","green","blue"];
 const btn = document.getElementById("my-button")
